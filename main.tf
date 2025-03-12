c3ee6cd517d8d6158392e2e6ee721e4476a0a515
Du-devBR <eduardo.ananiasdev@outlook.com>
2025-03-12 20:22:26 -0300

    feat: rodando pipeline para subir terraform localstack

--- main.tf
+++ main.tf
@@ -1,104 +1,55 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "docker" {}
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true # Ignora validação de credenciais
+  skip_requesting_account_id  = true # Ignora solicitação do ID da conta AWS
+  skip_metadata_api_check     = true # Ignora verificação de metadados da AWS
+  s3_use_path_style           = true # Necessário para compatibilidade com LocalStack
 
-# Criar a VPC principal
-resource "docker_network" "vpc_network" {
-  name   = "vpc_network"
-  driver = "bridge"
-}
-
-# Criar sub-rede pública
-resource "docker_network" "subnet_public" {
-  name   = "subnet_public"
-  driver = "bridge"
-}
-
-# Criar sub-rede privada
-resource "docker_network" "subnet_private" {
-  name   = "subnet_private"
-  driver = "bridge"
-}
-
-# Criar sub-rede de dados
-resource "docker_network" "subnet_data" {
-  name   = "subnet_data"
-  driver = "bridge"
-}
-
-# Criar um container na subrede pública (Nginx)
-resource "docker_container" "nginx_public" {
-  name  = "nginx_public"
-  image = "nginx"
-
-  networks_advanced {
-    name = docker_network.subnet_public.name
-  }
-  ports {
-    internal = 80
-    external = 80
+  # Define os endpoints do LocalStack para cada serviço AWS
+  endpoints {
+    ec2 = "http://localhost:4566"
   }
 }
 
-# Criar um container backend na subrede privada
-resource "docker_container" "backend_private" {
-  name    = "backend_private"
-  image   = "alpine"
-  command = ["sleep", "3600"]
+# Criar Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Permitir tráfego HTTP e SSH"
 
-  networks_advanced {
-    name = docker_network.subnet_private.name
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permitir SSH de qualquer lugar
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Permitir HTTP de qualquer lugar
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Permitir todo o tráfego de saída
   }
 }
 
-# Criar um container MySQL na subrede de dados
-resource "docker_container" "db_data" {
-  name  = "db_data"
-  image = "mysql"
-  env   = ["MYSQL_ROOT_PASSWORD=root"]
-
-  networks_advanced {
-    name = docker_network.subnet_data.name
-  }
-}
-
-# Criar um roteador que conecta todas as redes para permitir comunicação controlada
-resource "docker_container" "router" {
-  name    = "router"
-  image   = "alpine"
-  command = ["sh", "-c", "apk add iptables && sleep 3600"]
-
-  networks_advanced {
-    name = docker_network.subnet_public.name
-  }
-
-  networks_advanced {
-    name = docker_network.subnet_private.name
-  }
-
-  networks_advanced {
-    name = docker_network.subnet_data.name
-  }
-
-  capabilities {
-    add = ["NET_ADMIN"]
-  }
-}
-resource "null_resource" "configure_router" {
-  depends_on = [docker_container.router]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      sleep 5
-      docker exec router sh -c 'iptables -A FORWARD -s 192.168.192.0/20 -d 192.169.192.0/20 -j ACCEPT;
-      iptables -A FORWARD -s 192.168.160.0/20 -d 192.168.162.0/20 -j DROP;'
-EOT
-  }
+# Criar instância EC2 (simulada)
+resource "aws_instance" "ec2_instance" {
+  ami             = "ami-0c55b159cbfafe1f0" # AMI fictícia (LocalStack não valida)
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.ec2_sg.name] # Associar o Security Group
 }
